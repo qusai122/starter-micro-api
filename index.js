50787d00edc3c809ab66deed962c0425b9dd3cf8
qusai122 <qusai.2010.123@hotmail.com>
2023-07-17 15:40:45 +0300

    push to main

--- index.js
+++ index.js
@@ -1,27 +1,303 @@
-var http = require("http");
+const express = require("express");
+const app = express();
 
-http
-  .createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`);
+app.get("/products", (req, res) => {
+  console.log("Just got a GET request!");
+  return res.status(200).json([
+    {
+      id: 1,
+      name: "Zara simple T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 19.99,
+      quantity: 100,
+      rating: 4.7,
+      discount: 0,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+    {
+      id: 2,
+      name: "second Zara T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 29.99,
+      quantity: 10,
+      rating: 4.4,
+      discount: 15,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+    {
+      id: 3,
+      name: "old product",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 29.99,
+      quantity: 10,
+      rating: 4.4,
+      discount: 20,
+      createdAt: "2023-04-10T23:31:34.000Z",
+      updatedAt: "2023-04-10T23:31:34.000Z",
+      brand_id: 2,
+      category_id: 2,
+    },
+  ]);
+});
 
-    if (req.method === "GET") {
-      // Handle GET request
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.write("This is a GET request!");
-      res.end();
-    } else if (req.method === "POST") {
-      // Handle POST request
-      let body = "";
-      req.on("data", (chunk) => {
-        body += chunk;
-      });
+app.get("/products/limited", (req, res) => {
+  return res.status(200).json([
+    {
+      id: 2,
+      name: "second Zara T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 29.99,
+      quantity: 10,
+      rating: 4.4,
+      discount: 15,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+    {
+      id: 3,
+      name: "old product",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 29.99,
+      quantity: 10,
+      rating: 4.4,
+      discount: 20,
+      createdAt: "2023-04-10T23:31:34.000Z",
+      updatedAt: "2023-04-10T23:31:34.000Z",
+      brand_id: 2,
+      category_id: 2,
+    },
+  ]);
+});
 
-      req.on("end", () => {
-        // Process the POST request body
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.write(`This is a POST request! Body: ${body}`);
-        res.end();
-      });
-    }
-  })
-  .listen(process.env.PORT || 3000);
+app.get("/products/handpicked", (req, res) => {
+  return res.status(200).json([
+    {
+      id: 1,
+      name: "Zara simple T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 19.99,
+      quantity: 100,
+      rating: 4.7,
+      discount: 0,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+  ]);
+});
+
+app.get("/products/popular", (req, res) => {
+  return res.status(200).json([
+    {
+      id: 1,
+      name: "Zara simple T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 19.99,
+      quantity: 100,
+      rating: 4.7,
+      discount: 0,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+  ]);
+});
+
+app.get("/products/new", (req, res) => {
+  return res.status(200).json([
+    {
+      id: 1,
+      name: "Zara simple T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 19.99,
+      quantity: 100,
+      rating: 4.7,
+      discount: 0,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+    {
+      id: 2,
+      name: "second Zara T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 29.99,
+      quantity: 10,
+      rating: 4.4,
+      discount: 15,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+  ]);
+});
+
+app.get("/products/search", (req, res) => {
+  return res.status(200).json([
+    {
+      id: 1,
+      name: "Zara simple T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 19.99,
+      quantity: 100,
+      rating: 4.7,
+      discount: 0,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+      brand: {
+        id: 1,
+        name: "Zara",
+        description: "clothing company",
+        img: "https://logos-world.net/zara-logo/",
+      },
+    },
+    {
+      id: 2,
+      name: "second Zara T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 29.99,
+      quantity: 10,
+      rating: 4.4,
+      discount: 15,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+      brand: {
+        id: 1,
+        name: "Zara",
+        description: "clothing company",
+        img: "https://logos-world.net/zara-logo/",
+      },
+    },
+  ]);
+});
+app.get("/products/categories", (req, res) => {
+  return res.status(200).json([
+    {
+      id: 1,
+      name: "T-shirts",
+      description: "no need to describe a T-shirts category :)",
+      img: "https://blog.treasurie.com/types-of-t-shirts/",
+    },
+    {
+      id: 2,
+      name: "test categorey",
+      description: "qqqqqqqqqqqqqqqqqqqq",
+      img: null,
+    },
+  ]);
+});
+app.get("/products/brands", (req, res) => {
+  return res.status(200).json([
+    {
+      id: 1,
+      name: "Zara",
+      description: "clothing company",
+      img: "https://logos-world.net/zara-logo/",
+    },
+    {
+      id: 2,
+      name: "my brand",
+      description: "non of your bussnis",
+      img: null,
+    },
+  ]);
+});
+app.get("/brands/products/1", (req, res) => {
+  return res.status(200).json([
+    {
+      id: 1,
+      name: "Zara simple T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 19.99,
+      quantity: 100,
+      rating: 4.7,
+      discount: 0,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+    {
+      id: 2,
+      name: "second Zara T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 29.99,
+      quantity: 10,
+      rating: 4.4,
+      discount: 15,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+  ]);
+});
+app.get("/categories/products/1", (req, res) => {
+  return res.status(200).json([
+    {
+      id: 1,
+      name: "Zara simple T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 19.99,
+      quantity: 100,
+      rating: 4.7,
+      discount: 0,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+    {
+      id: 2,
+      name: "second Zara T-shirt",
+      tittle: "Zara T-shirt",
+      description: "casual T-shirt ",
+      price: 29.99,
+      quantity: 10,
+      rating: 4.4,
+      discount: 15,
+      createdAt: "2023-07-10T23:31:34.000Z",
+      updatedAt: "2023-07-10T23:31:34.000Z",
+      brand_id: 1,
+      category_id: 1,
+    },
+  ]);
+});
+app.use((req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
